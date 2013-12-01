b4284ada3c7a83e06165a4802e0565554a2478e0
Poweruser <poweruser.rs@hotmail.com>
2013-12-01 16:27:03 +0100

    Spigots fix for the Zombie lag of 1.6, further tweaked by me

--- src/main/java/net/minecraft/server/PathfinderGoalMeleeAttack.java
+++ src/main/java/net/minecraft/server/PathfinderGoalMeleeAttack.java
@@ -13,6 +13,10 @@ public class PathfinderGoalMeleeAttack extends PathfinderGoal {
     Class g;
     private int h;
 
+    // Poweruser
+    private int failedPathFindingPenalty = 10;
+    private int penalty = 4;
+
     public PathfinderGoalMeleeAttack(EntityCreature entitycreature, Class oclass, double d0, boolean flag) {
         this(entitycreature, d0, flag);
         this.g = oclass;
@@ -36,8 +40,35 @@ public class PathfinderGoalMeleeAttack extends PathfinderGoal {
         } else if (this.g != null && !this.g.isAssignableFrom(entityliving.getClass())) {
             return false;
         } else {
-            this.f = this.b.getNavigation().a(entityliving);
-            return this.f != null;
+            // Poweruser start
+            if(--h <= 0) {
+                double xdiff = entityliving.locX - this.b.locX;
+                double zdiff = entityliving.locZ - this.b.locZ;
+                if(!entityliving.onGround ||
+                        entityliving.boundingBox.b > this.b.boundingBox.b + 1.9 &&
+                        Math.sqrt(xdiff * xdiff + zdiff * zdiff) < 3) {
+                    return true;
+                }
+                this.f = this.b.getNavigation().a(entityliving);
+                boolean success = false;
+                if(this.b.getNavigation().e() != null) {
+                    PathPoint finalPathPoint = this.b.getNavigation().e().c();
+                    if(finalPathPoint != null) {
+                        double diff = entityliving.f(finalPathPoint.a, finalPathPoint.b, finalPathPoint.c);
+	                    success = diff < 1.5;
+                    }
+                }
+                if(success) {
+                    this.failedPathFindingPenalty = 0;
+                } else if(this.failedPathFindingPenalty < 60) {
+                    this.failedPathFindingPenalty += penalty;
+                }
+                this.h = this.failedPathFindingPenalty + 4 + this.b.aC().nextInt(7);
+                return this.f != null;
+            } else {
+                return true;
+            }
+            // Poweruser end
         }
     }
 
@@ -63,19 +94,105 @@ public class PathfinderGoalMeleeAttack extends PathfinderGoal {
         this.b.getNavigation().h();
     }
 
+    // Spigot start
+    private double pathX;
+    private double pathY;
+    private double pathZ;
+    private boolean prevPathOK;
+    private int fullRangeSearchDelay;
+    // Spigot end
     public void e() {
         EntityLiving entityliving = this.b.getGoalTarget();
 
         this.b.getControllerLook().a(entityliving, 30.0F, 30.0F);
+        double goalDistanceSq = this.b.e( entityliving.locX, entityliving.boundingBox.b, entityliving.locZ ); // Spigot
         if ((this.e || this.b.getEntitySenses().canSee(entityliving)) && --this.h <= 0) {
-            this.h = 4 + this.b.aC().nextInt(7);
-            this.b.getNavigation().a((Entity) entityliving, this.d);
+            // Spigot start
+            double targetMovement = entityliving.e( pathX, pathY, pathZ );
+            // If this is true, then we are re-pathing
+            if ( ( this.h <= 0 && targetMovement >= 1.0D ) || ( this.h <= 0 && this.b.aC().nextInt( 200 ) == 0 ) ) /* EntityCreature random instance */
+
+            {
+                AttributeInstance rangeAttr = this.b.getAttributeInstance( GenericAttributes.b );
+                double origRange = rangeAttr.getValue();
+                if ( fullRangeSearchDelay > 0 )
+                {
+
+                    double dist = Math.sqrt( goalDistanceSq );
+                    if ( dist <= 8.0D )
+                    {
+                        dist = 8.0D;
+                    }
+                    if ( dist > origRange )
+                    {
+                        dist = origRange;
+                    }
+                    rangeAttr.setValue( dist );
+                }
+
+                prevPathOK = this.b.getNavigation().a( (Entity) entityliving, this.d );
+
+                if ( fullRangeSearchDelay > 0 )
+                {
+                    fullRangeSearchDelay--;
+                    if ( origRange > 40.0D )
+                    {
+                        origRange = 40.0D;
+                    }
+                    rangeAttr.setValue( origRange );
+                }
+
+                pathX = entityliving.locX;
+                pathY = entityliving.boundingBox.b;
+                pathZ = entityliving.locZ;
+
+                // Poweruser start
+                boolean success = false;
+                if(this.b.getNavigation().e() != null) {
+                    PathPoint finalPathPoint = this.b.getNavigation().e().c();
+                    if(finalPathPoint != null) {
+                        double diff = entityliving.f(finalPathPoint.a, finalPathPoint.b, finalPathPoint.c);
+                        success = diff < 1.0;
+                    }
+                }
+                if(success) {
+                    this.failedPathFindingPenalty = 0;
+                } else if(failedPathFindingPenalty < 60) {
+                    this.failedPathFindingPenalty += penalty;
+                }
+                this.h = this.failedPathFindingPenalty + 4 + this.b.aC().nextInt(7);
+                // Poweruser end
+                //this.h = 4 + this.b.aC().nextInt( 7 ); /* EntityCreature random instance */
+
+                if ( goalDistanceSq > 256.0D )
+                {
+                    if ( goalDistanceSq > 1024.0D )
+                    {
+                        this.h += 8;
+                    } else
+                    {
+                        this.h += 16;
+                    }
+                } else if ( !prevPathOK )
+                {
+                    this.h += 24;
+                }
+
+                if ( !prevPathOK || goalDistanceSq <= 256.0D )
+                {
+                    if ( fullRangeSearchDelay <= 0 )
+                    {
+                        fullRangeSearchDelay = 4 + this.b.aC().nextInt( 4 ); /* EntityCreature random instance */
+                    }
+                }
+            }
         }
+        // Spigot end
 
         this.c = Math.max(this.c - 1, 0);
         double d0 = (double) (this.b.width * 2.0F * this.b.width * 2.0F + entityliving.width);
 
-        if (this.b.e(entityliving.locX, entityliving.boundingBox.b, entityliving.locZ) <= d0) {
+        if (goalDistanceSq <= d0) { // Spigot
             if (this.c <= 0) {
                 this.c = 20;
                 if (this.b.aY() != null) {
