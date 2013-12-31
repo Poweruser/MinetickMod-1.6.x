8c5d4cb8462c62e0ee8d4d8c69bbc9d246049de8
Poweruser <poweruser.rs@hotmail.com>
2013-12-31 01:19:54 +0100

    Only add loaded chunks to the ticklist. I dont want the WorldServers to generate chunks in doTick(), only the world threads are allowed to do that

--- src/main/java/net/minecraft/server/World.java
+++ src/main/java/net/minecraft/server/World.java
@@ -2119,8 +2119,9 @@ public abstract class World implements IBlockAccess {
                         continue;
                     }
                     // CraftBukkit end
-
-                    this.chunkTickList.add(org.bukkit.craftbukkit.util.LongHash.toLong(l + j, i1 + k)); // CraftBukkit
+                    if(chunkProviderServer.isChunkLoaded(l + j, i1 + k)) { // Poweruser
+                        this.chunkTickList.add(org.bukkit.craftbukkit.util.LongHash.toLong(l + j, i1 + k)); // CraftBukkit
+                    }
                 }
             }
         }
