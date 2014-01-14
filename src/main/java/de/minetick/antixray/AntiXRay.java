cc27b26f2ca59ff2f0603b6cd2830f4388a9ba4f
Poweruser <poweruser.rs@hotmail.com>
2014-01-14 23:42:15 +0100

    Updating range of surrounding orebfuscated blocks increased by 1 block in x and z direction. Some players were noticing the fake blocks during normal mining. The cause is probably a slow client connection, or the server is not sending the updates fast enough

--- src/main/java/de/minetick/antixray/AntiXRay.java
+++ src/main/java/de/minetick/antixray/AntiXRay.java
@@ -28,7 +28,8 @@ public class AntiXRay {
                                                                   {-2,0, 0},{-2,0, 1},{-2,0,-1},
                                                                   { 0,0,-2},{ 1,0,-2},{-1,0,-2},
                                                                   { 0,0, 2},{ 1,0, 2},{-1,0, 2},
-                                                                  { 0,-2,0},{ 0,2, 0}};
+                                                                  { 0,-2,0},{ 0,2, 0},{ 3,0, 0},
+                                                                  {-3, 0,0},{ 0,0, 3},{ 0,0,-3}};
     private static List<String> configWorlds = new LinkedList<String>();
     private Random random = new Random();
     private static ScheduledExecutorService scheduledExecutorService;
