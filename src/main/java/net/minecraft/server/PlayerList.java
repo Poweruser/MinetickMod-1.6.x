28e64b28310cdc6bb1cac1f663727365b5901e70
Poweruser <poweruser.rs@hotmail.com>
2014-01-31 22:56:56 +0100

    Correction of f8f476df8e: Wrong packet class

--- src/main/java/net/minecraft/server/PlayerList.java
+++ src/main/java/net/minecraft/server/PlayerList.java
@@ -720,7 +720,7 @@ public abstract class PlayerList {
                     this.indexCounter = 0;
                 }
                 EntityPlayer entityplayer = (EntityPlayer) this.players.get(this.indexCounter);
-                Packet packet = new PacketPlayOutPlayerInfo(entityplayer.listName, true, entityplayer.ping);
+                Packet packet = new Packet201PlayerInfo(entityplayer.listName, true, entityplayer.ping);
                 for (int i = 0; i < this.players.size(); ++i) {
                     EntityPlayer receiver = ((EntityPlayer) this.players.get(i));
                     if (receiver.getBukkitEntity().canSee(entityplayer.getBukkitEntity())) {
